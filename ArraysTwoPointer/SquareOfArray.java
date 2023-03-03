9c3cf8e2dc874a2c4936f71f1a1132c93731ad6f
sahil <sahil.gohil@yahoo.in>
2023-03-03 12:52:06 +1100

    matrix sum

--- ArraysTwoPointer/SquareOfArray.java
+++ ArraysTwoPointer/SquareOfArray.java
@@ -1,11 +1,5 @@
 package ArraysTwoPointer;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-import ArrayUtil;
 
 public class SquareOfArray {
     
